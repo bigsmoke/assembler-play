f3ea674a4e6e2e52f8d9ea7277528954d0ad4bc1
Wiebe Cazemier <wiebe@halfgaar.net>
2009-10-31 19:48:08 +0100

    I have a working version of showimage, but with the wrong palette.

--- showimage.asm
+++ showimage.asm
@@ -1,8 +1,5 @@
 segment .data
 
-; This should be the location of the first pixel, skipping the BMP header and pallete.
-pixel_pointer dw 0
-
 segment .text
 
 start:
@@ -21,6 +18,7 @@ load_image_to_mem
     mov ax, 2000h       ; ES:BX = 2000:0000
     mov es, ax          ;
     mov bx, 0
+    mov ds, ax          ; Set the datasegment to this segment, so that future reads will use this segment
     mov ah, 2           ; Load disk data (to ES:BX)
     mov al, 128         ; Load 128 sectors, 64 kbyte
     mov ch, 0           ; Cylinder=0
@@ -33,18 +31,17 @@ load_image_to_mem
     mov ah, 0x0c ; write pixel
     mov cx, 0 ; x
     mov dx, 199 ; y
-    mov al, 5 ; color will be purple if things go wrong
+    ;mov bx, 438h ; If I use pixel_pointer, it doesn't start at the right place, it seems
+    mov bx, 110h ; If I use pixel_pointer, it doesn't start at the right place, it seems
+    mov al, [bx]
 
 draw_next_pixel:
     cmp cx, 320
     jz goto_next_line
 
-    mov al, [pixel_pointer] ; color
     int 10h
-
-    mov bx, [pixel_pointer]
     inc bx
-    mov [pixel_pointer], bx
+    mov al, [bx]
 
     inc cx ; x pos
     jmp draw_next_pixel
@@ -53,7 +50,7 @@ goto_next_line:
     cmp dx, 0
     jz hang
 
-    dec dx ; y pos
+    dec dx ; y pos. Because BMP's are bottom-up, it starts at the bottom and then decreases.
     mov cx, 0
     jmp draw_next_pixel
 
@@ -69,7 +66,5 @@ hang:                   ; Hang!
     jmp hang
 
 ; Without this, it won't boot for some reason.
-; This and the bootloader are exactly 10 sectors long.
-; FIXME: for some reason, this cannot be tuned so that the output file is 4608
-; bytes long. It's 4606 or 4610.
+; This and the bootloader are exactly 10 sectors +2 bytes long.
 times 4608-($-$$) db 0
